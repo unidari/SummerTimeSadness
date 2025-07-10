d0b0d9eb31758183ae2b9674b2cbdf55655228c0
unidari <b.darya2809@gmail.com>
2025-07-11 01:23:08 +0300

    first

--- src/main.py
+++ src/main.py
@@ -1,177 +1,113 @@
 import flet as ft
-
-
-class CalcButton(ft.ElevatedButton):
-    def __init__(self, text, button_clicked, expand=1):
-        super().__init__()
-        self.text = text
-        self.expand = expand
-        self.on_click = button_clicked
-        self.data = text
-
-
-class DigitButton(CalcButton):
-    def __init__(self, text, button_clicked, expand=1):
-        CalcButton.__init__(self, text, button_clicked, expand)
-        self.bgcolor = ft.Colors.BLACK
-        self.color = ft.Colors.PINK
-
-
-class ActionButton(CalcButton):
-    def __init__(self, text, button_clicked):
-        CalcButton.__init__(self, text, button_clicked)
-        self.bgcolor = ft.Colors.BLACK
-        self.color = ft.Colors.PINK
-
-
-class ExtraActionButton(CalcButton):
-    def __init__(self, text, button_clicked):
-        CalcButton.__init__(self, text, button_clicked)
-        self.bgcolor = ft.Colors.WHITE
-        self.color = ft.Colors.PINK
-
-
-class CalculatorApp(ft.Container):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-
-        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
-        self.width = 350
-        self.bgcolor = ft.Colors.GREY
-        self.border_radius = ft.border_radius.all(20)
-        self.padding = 20
-        self.content = ft.Column(
-            controls=[
-                ft.Row(controls=[self.result], alignment="end"),
-                ft.Row(
-                    controls=[
-                        ExtraActionButton(
-                            text="AC", button_clicked=self.button_clicked
-                        ),
-                        ExtraActionButton(
-                            text="+/-", button_clicked=self.button_clicked
-                        ),
-                        ExtraActionButton(text="%", button_clicked=self.button_clicked),
-                        ActionButton(text="/", button_clicked=self.button_clicked),
-                    ]
-                ),
-                ft.Row(
-                    controls=[
-                        DigitButton(text="7", button_clicked=self.button_clicked),
-                        DigitButton(text="8", button_clicked=self.button_clicked),
-                        DigitButton(text="9", button_clicked=self.button_clicked),
-                        ActionButton(text="*", button_clicked=self.button_clicked),
-                    ]
-                ),
-                ft.Row(
-                    controls=[
-                        DigitButton(text="4", button_clicked=self.button_clicked),
-                        DigitButton(text="5", button_clicked=self.button_clicked),
-                        DigitButton(text="6", button_clicked=self.button_clicked),
-                        ActionButton(text="-", button_clicked=self.button_clicked),
-                    ]
-                ),
-                ft.Row(
-                    controls=[
-                        DigitButton(text="1", button_clicked=self.button_clicked),
-                        DigitButton(text="2", button_clicked=self.button_clicked),
-                        DigitButton(text="3", button_clicked=self.button_clicked),
-                        ActionButton(text="+", button_clicked=self.button_clicked),
-                    ]
-                ),
-                ft.Row(
-                    controls=[
-                        DigitButton(
-                            text="0", expand=2, button_clicked=self.button_clicked
-                        ),
-                        DigitButton(text=".", button_clicked=self.button_clicked),
-                        ActionButton(text="=", button_clicked=self.button_clicked),
-                    ]
-                ),
-            ]
-        )
-
-    def button_clicked(self, e):
-        data = e.control.data
-        print(f"Кнопка нажата со следующими параметрами = {data}")
-        if self.result.value == "Error" or data == "AC":
-            self.result.value = "0"
-            self.reset()
-
-        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
-            if self.result.value == "0" or self.new_operand == True:
-                self.result.value = data
-                self.new_operand = False
-            else:
-                self.result.value = self.result.value + data
-
-        elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.operator = data
-            if self.result.value == "Error":
-                self.operand1 = "0"
-            else:
-                self.operand1 = float(self.result.value)
-            self.new_operand = True
-
-        elif data in ("="):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.reset()
-
-        elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
-            self.reset()
-
-        elif data in ("+/-"):
-            if float(self.result.value) > 0:
-                self.result.value = "-" + str(self.result.value)
-
-            elif float(self.result.value) < 0:
-                self.result.value = str(
-                    self.format_number(abs(float(self.result.value)))
-                )
-
-        self.update()
-
-    def format_number(self, num):
-        if num % 1 == 0:
-            return int(num)
-        else:
-            return num
-
-    def calculate(self, operand1, operand2, operator):
-
-        if operator == "+":
-            return self.format_number(operand1 + operand2)
-
-        elif operator == "-":
-            return self.format_number(operand1 - operand2)
-
-        elif operator == "*":
-            return self.format_number(operand1 * operand2)
-
-        elif operator == "/":
-            if operand2 == 0:
-                return "Error"
-            else:
-                return self.format_number(operand1 / operand2)
-
-    def reset(self):
-        self.operator = "+"
-        self.operand1 = 0
-        self.new_operand = True
-
+import random
+import time
 
 def main(page: ft.Page):
-    page.title = "Калькулятор"
-    calc = CalculatorApp()
+    page.title = "Игра Найди пары"
+    page.window_height = 710
+    page.window_width = 910
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    page.add(calc)
+    number_of_single_cards = 20
+    image_page = "https://picsum.photos/id/"
 
+    def make_cards(number_of_single_cards):
+        card_deck_with_duplicates = \
+            [f"{i}_A" for i in range(number_of_single_cards)] + \
+            [f"{i}_B" for i in range(number_of_single_cards)]
+        random.shuffle(card_deck_with_duplicates)
+        return card_deck_with_duplicates
 
-ft.app(target=main)
+    idx_of_cards = make_cards(number_of_single_cards)
+    list_of_pairs = []
+    find = []
+
+    def start_game(e):
+        e.control.disabled = True
+        global start_time
+        start_time = time.time()
+
+        for i in range(len(idx_of_cards)):
+            row_board.controls[i].content = None
+            row_board.controls[i].bgcolor = ft.Colors.AMBER
+            row_board.controls[i].border_radius = ft.border_radius.all(10)
+            row_board.controls[i].disabled = False
+        page.update()
+
+    def match_pairs(e):
+        if len(list_of_pairs) == 2 or not start_button.disabled:
+            return
+
+        num = e.control.data
+        idx = idx_of_cards[num].split('_')[0]
+        index_a = idx_of_cards.index(f"{idx}_A")
+        index_b = idx_of_cards.index(f"{idx}_B")
+        index_of_image = idx_of_cards[num].split("_")[0]
+
+        if len(list_of_pairs) < 2:
+            e.control.content = ft.Image(src=f"{image_page+'1'+index_of_image}/100",
+                                        border_radius=ft.border_radius.all(10))
+            if idx_of_cards[num] not in list_of_pairs:
+                list_of_pairs.append(idx_of_cards[num])
+            page.update()
+
+        if len(list_of_pairs) == 2:
+            a = int(list_of_pairs[0].split('_')[0])
+            b = int(list_of_pairs[1].split('_')[0])
+
+            if a == b:
+                time.sleep(0.4)
+                for index in (index_a, index_b):
+                    row_board.controls[index].disabled = True
+                    row_board.controls[index].content = None
+                    row_board.controls[index].bgcolor = ft.Colors.BLACK
+                find.append(index_a)
+                if len(find) == number_of_single_cards:
+                    stop_time = time.time()
+                    start_button.text = f"Время: \n{round(stop_time - start_time, 0)} \nсекунд"
+                    page.update()
+                page.update()
+                list_of_pairs.clear()
+            else:
+                time.sleep(1)
+                flip_a = idx_of_cards.index(list_of_pairs[0])
+                flip_b = idx_of_cards.index(list_of_pairs[1])
+                for index in (flip_a, flip_b):
+                    row_board.controls[index].content = None
+                    row_board.controls[index].bgcolor = ft.Colors.AMBER
+                list_of_pairs.clear()
+                page.update()
+
+    def board(idx_of_cards):
+        card_images = []
+        for num in range(len(idx_of_cards)):
+            index_of_image = idx_of_cards[num].split("_")[0]
+            card_images.append(
+                ft.Container(
+                    content=None,
+                    width=100,
+                    height=100,
+                    bgcolor=ft.Colors.AMBER,
+                    border_radius=ft.border_radius.all(10),
+                    data=num,
+                    on_click=match_pairs
+                )
+            )
+        return card_images
+
+    row_board = ft.Row(wrap=True, controls=board(idx_of_cards))
+    start_button = ft.ElevatedButton(text="Начать игру", width=100, height=100, on_click=start_game)
+
+    page.add(
+        ft.Column(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.Text(value="Найди пары", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+                row_board,
+                start_button
+            ]
+        )
+    )
+
+ft.app(main)
