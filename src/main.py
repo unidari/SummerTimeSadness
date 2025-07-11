183bae90114b1dff462952c2ea0c94284a91c4fe
unidari <b.darya2809@gmail.com>
2025-07-11 11:19:15 +0300

    yoy

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def main(page: ft.Page):
     page.title = "Игра Найди пары"
     page.window_height = 710
     page.window_width = 910
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     number_of_single_cards = 20
     image_page = "https://picsum.photos/id/"
@@ -21,12 +23,26 @@ def main(page: ft.Page):
     list_of_pairs = []
     find = []
 
+    def start_game(e):
+        e.control.disabled = True
+        global start_time
+        start_time = time.time()
+
+        for i in range(len(idx_of_cards)):
+            row_board.controls[i].content = None
+            row_board.controls[i].bgcolor = ft.Colors.AMBER
+            row_board.controls[i].border_radius = ft.border_radius.all(10)
+            row_board.controls[i].disabled = False
+        page.update()
+
     def match_pairs(e):
-        if len(list_of_pairs) == 2:
+        if len(list_of_pairs) == 2 or not start_button.disabled:
             return
 
         num = e.control.data
         idx = idx_of_cards[num].split('_')[0]
+        index_a = idx_of_cards.index(f"{idx}_A")
+        index_b = idx_of_cards.index(f"{idx}_B")
         index_of_image = idx_of_cards[num].split("_")[0]
 
         if len(list_of_pairs) < 2:
@@ -42,13 +58,15 @@ def main(page: ft.Page):
 
             if a == b:
                 time.sleep(0.4)
-                index_a = idx_of_cards.index(str(idx)+'_A')
-                index_b = idx_of_cards.index(str(idx)+'_B')
                 for index in (index_a, index_b):
                     row_board.controls[index].disabled = True
                     row_board.controls[index].content = None
                     row_board.controls[index].bgcolor = ft.Colors.BLACK
                 find.append(index_a)
+                if len(find) == number_of_single_cards:
+                    stop_time = time.time()
+                    start_button.text = f"Время: \n{round(stop_time - start_time, 0)} \nсекунд"
+                    page.update()
                 page.update()
                 list_of_pairs.clear()
             else:
@@ -79,6 +97,17 @@ def main(page: ft.Page):
         return card_images
 
     row_board = ft.Row(wrap=True, controls=board(idx_of_cards))
-    page.add(row_board)
+    start_button = ft.ElevatedButton(text="Начать игру", width=100, height=100, on_click=start_game)
+
+    page.add(
+        ft.Column(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.Text(value="Найди пары", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+                row_board,
+                start_button
+            ]
+        )
+    )
 
 ft.app(main)
